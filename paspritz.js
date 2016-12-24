681a3289c0bcda04ed5f569fa1e99c1526aa4dd8
bwzheng <bob.zheng1998@gmail.com>
2016-12-23 21:35:46 -0800

    Implemented chatbot

--- paspritz.js
+++ paspritz.js
@@ -4,6 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 var randomstring = require("randomstring");
+var PAGE_ACCESS_TOKEN = "EAARMDbQZCF4MBAFa0dOGjpPKlZBH7uZAu5vsOx54qF1OpoK36D6TlyLXkFqorkfApSZAfRb7ixrCfOHA0wvV3XHWrj5bItaJ9w1CQIWVKKuKRGxSnj6wmbI8NYSCVtSZA1ChjHV1QRbSsqqC8pi2dNfBvMpWhakSjIvqtMlemlQZDZD"
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -36,43 +37,170 @@ app.get('/webhook', function(req, res) {
 });
 
 app.post('/webhook', function (req, res) {
-  res.send({
-    speech: "Just a second",
-    displayText: "Just a second"
-  });
-  var intent = req.body.result.metadata.intentName
-  var params = req.body.result.parameters
-  if (intent == "Do url"){
-    request(params.url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var data = extractor(body);
-        spritzify(data.text, function () {
-          imgur.uploadFile(gifname)
-            .then(function (json) {
-                console.log(json.data.link);
-                fs.unlinkSync(gifname);
-                res.send({
-                  speech: "Here's the article",
-                  displayText: json.data.link,
-                  source: "Imgur"
-                });
-            })
-            .catch(function (err) {
-                console.error(err.message);
-                fs.unlinkSync(gifname);
-                res.send({
-                  speech: "Something's wrong, please try again.",
-                  displayText: "Something's wrong, please try again.",
-                });
-            });
-        });
+  var data = req.body;
 
-      }
-    })
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
   }
+});
 
+function sendGenericMessage(recipientId, messageText) {
+  var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [{
+              title: "rift",
+              subtitle: "Next-generation virtual reality",
+              item_url: "https://www.oculus.com/en-us/rift/",
+              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/rift/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for first bubble",
+              }],
+            }, {
+              title: "touch",
+              subtitle: "Your Hands, Now in VR",
+              item_url: "https://www.oculus.com/en-us/touch/",
+              image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/touch/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for second bubble",
+              }]
+            }]
+          }
+        }
+      }
+    };
 
-})
+    callSendAPI(messageData);
+  }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+  var messageId = message.mid;
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+  //console.log("Message data: ", event.message);
+  //request(params.url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = extractor(body);
+      spritzify(data.text, function () {
+        imgur.uploadFile(gifname)
+          .then(function (json) {
+              console.log(json.data.link);
+              fs.unlinkSync(gifname);
+              res.send({
+                speech: "Here's the article",
+                displayText: json.data.link,
+                source: "Imgur"
+              });
+          })
+          .catch(function (err) {
+              console.error(err.message);
+              fs.unlinkSync(gifname);
+              res.send({
+                speech: "Something's wrong, please try again.",
+                displayText: "Something's wrong, please try again.",
+              });
+          });
+      });
+
+    }
+  })
+}
 
 function spritzify(input, callback){
 
