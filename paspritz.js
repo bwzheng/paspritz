7bfcb26d25e3c46136c09f08759fd2741445c0a6
bwzheng <bob.zheng1998@gmail.com>
2016-12-27 15:35:57 -0800

    0333

--- paspritz.js
+++ paspritz.js
@@ -284,7 +284,7 @@ function spritzify(input){
       encoder.addFrame(ctx);
     }
     encoder.finish();
-    console.log("finished");
+    console.log(gifname);
     // var currentWord = 0;
     // var running = true;
     // var spritz_timers = new Array();
