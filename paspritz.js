cf524e20b752ee55726a24d02b8d755d19b6c947
bwzheng <bob.zheng1998@gmail.com>
2016-12-28 00:38:38 -0800

    1238

--- paspritz.js
+++ paspritz.js
@@ -30,192 +30,25 @@ myImage.src = 'download.png';
 //Lets define a port we want to listen to
 var port = process.env.PORT || 3000;
 
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'passenger_avenue') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
-
-app.post('/webhook', function (req, res) {
+app.post('/message', function (req, res) {
   var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
+  request(data.url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = extractor(body);
+      spritzify(data.text, function(){
+        cloudinary.uploader.upload(gifname, function(result) {
+          console.log(JSON.stringify(result, null, 4))
+          fs.unlinkSync(gifname);
+          res.json({link: result.url});
+          res.sendStatus(200);
+        })
       });
-    });
 
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
+    }
+  })
 });
 
-function sendGenericMessage(recipientId, messageText) {
-  var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [{
-              title: "rift",
-              subtitle: "Next-generation virtual reality",
-              item_url: "https://www.oculus.com/en-us/rift/",
-              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for first bubble",
-              }],
-            }, {
-              title: "touch",
-              subtitle: "Your Hands, Now in VR",
-              item_url: "https://www.oculus.com/en-us/touch/",
-              image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/touch/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for second bubble",
-              }]
-            }]
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-
-}
-
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function sendAttachmentMessage(recipientId, messageText){
-  var messageData = {
-    recipient:{
-      id:recipientId
-    },
-    "message":{
-      "attachment":{
-        "type":"image",
-        "payload":{
-          "url":messageText
-        }
-      }
-    }
-  }
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      // console.error(response);
-      // console.error(error);
-    }
-  });
-}
-
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-  var messageId = message.mid;
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  console.log(messageAttachments);
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    if (messageAttachments[0].hasOwnProperty('url')) {
-      request(messageAttachments[0].url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var data = extractor(body);
-          spritzify(data.text);
-          cloudinary.uploader.upload(gifname, function(result) {
-            console.log(JSON.stringify(result, null, 4))
-            fs.unlinkSync(gifname);
-            sendAttachmentMessage(senderID, result.url);
-          })
-        }
-      })
-    } else {
-      sendTextMessage(senderID, "Message with attachment received");
-    }
-
-  }
-  //console.log("Message data: ", event.message);
-
-}
-
-function spritzify(input){
+function spritzify(input, callback){
 
     var wpm = 300;
     var ms_per_word = 60000/wpm;
@@ -284,55 +117,7 @@ function spritzify(input){
       encoder.addFrame(ctx);
     }
     encoder.finish();
-    console.log(gifname);
-    // var currentWord = 0;
-    // var running = true;
-    // var spritz_timers = new Array();
-    //
-    // document.getElementById("spritz_toggle").addEventListener("click", function() {
-    //     if(running) {
-    //         stopSpritz();
-    //     } else {
-    //         startSpritz();
-    //     }
-    // });
-
-    // function updateValues(i) {
-    //
-    //     var p = pivot(all_words[i]);
-    //     // document.getElementById("spritz_result").innerHTML = p;
-    //     currentWord = i;
-    //     return p
-    //
-    // }
-
-    // function startSpritz() {
-    //
-    //     document.getElementById("spritz_toggle").style.display = "block";
-    //     document.getElementById("spritz_toggle").textContent = "Pause";
-    //
-    //     running = true;
-    //
-    //     spritz_timers.push(setInterval(function() {
-    //         updateValues(currentWord);
-    //         currentWord++;
-    //         if(currentWord >= all_words.length) {
-    //             currentWord = 0;
-    //             stopSpritz();
-    //         }
-    //     }, ms_per_word));
-    // }
-    //
-    // function stopSpritz() {
-    //     for(var i = 0; i < spritz_timers.length; i++) {
-    //         clearTimeout(spritz_timers[i]);
-    //     }
-    //
-    //     document.getElementById("spritz_toggle").textContent = "Play";
-    //     running = false;
-    // }
-    //
-    // startSpritz();
+    callback();
 }
 
 // Find the red-character of the current word.
